4037dbf0c820594e750dfab41ef0b3147b58aeb2
dirkarnez <smalldirkalex@gmail.com>
2024-10-05 22:19:20 +0800

    - working

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-doc = pymupdf.open("output.pdf")  # re-open PDF
+import pymupdf # imports the pymupdf library
+
+doc = pymupdf.open("./tests/10064689564_0.pdf")  # re-open PDF
 page = doc[0]  # first page
-page.get_drawings()  # extract its drawings
+list_of_drawings = page.get_drawings()  # extract its drawings
+
+# rgb(68, 114, 196)
+
+list_of_drawings_bluey = [draw for draw in list_of_drawings 
+    if draw['fill'] != None 
+    if len(draw['fill']) == 3
+    if draw['fill'][2] >  draw['fill'][1] 
+    if draw['fill'][2] >  draw['fill'][0] 
+    if draw['fill_opacity'] == 1 
+    ]
+a = len(list_of_drawings)
+b = len(list_of_drawings_bluey)
+
+# b is expected to be 3
+
+print(list_of_drawings_bluey)
+
+#  (0.6000000238418579, 0.6000000238418579, 0.6000000238418579)
 
 # Out[14]:
-# [{
+# [
 # 'items': [  # draw commands are here
 #   ('re', Rect(100.0, 100.0, 200.0, 200.0), 1),  # rect from above
 #   ('l', Point(100.0, 200.0), Point(200.0, 100.0)),  # first line
